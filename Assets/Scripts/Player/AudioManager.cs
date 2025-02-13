f155466fed4d13add3b3f9abc1f20dfabb6c82d5
DArkPhenix <eduardo.huerta@saei.mx>
2025-02-12 20:59:19 -0600

    12-02-2025
    Add audio to chest interaction
    Add key uses - remove from inventory
    Fix key movement errors
    Add layers for player, ground and enemies
    Add jump - missing grounded logic

--- Assets/Scripts/Player/AudioManager.cs
+++ Assets/Scripts/Player/AudioManager.cs
@@ -14,47 +14,36 @@ public class AudioManager : MonoBehaviour
     public List<AudioClip> _sword = new List<AudioClip>();
     
     public AudioClip _ChargeClip;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     public void PlayWalkSound()
     {
         if (!_soundEffects.isPlaying)
         { 
-            var i = Random.Range(0, _walk.Count);
-            _soundEffects.clip = _walk[i];
+            _soundEffects.pitch = Random.Range(0.9f, 1.1f);
+            _soundEffects.clip = _walk[Random.Range(0, _walk.Count)];
             _soundEffects.Play();
         }
     }
 
     public void PlayAttackSound()
     {
-        var i = 0;
         if (!_voiceLines.isPlaying)
         {
-            i = Random.Range(0, _gruntsClips.Count);
-            _voiceLines.clip = _gruntsClips[i];
+            _voiceLines.pitch = Random.Range(0.9f, 1.1f); 
+            _voiceLines.clip = _gruntsClips[Random.Range(0, _gruntsClips.Count)];
             _voiceLines.Play();
         }
 
         _swordSource.Pause(); 
-        i = Random.Range(0, _sword.Count);
-        _swordSource.clip = _sword[i];
+        _swordSource.pitch = Random.Range(0.9f, 1.1f); 
+        _swordSource.clip = _sword[ Random.Range(0, _sword.Count)];
         _swordSource.Play();
        
     }
 
     public void PlayChargedAtackSound()
     {
+        _swordSource.pitch = Random.Range(0.9f, 1.1f); 
         _swordSource.Pause();
         _swordSource.clip = _ChargeClip;
         _swordSource.Play();
